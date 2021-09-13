3ac925c42c3afe6d7904e2be5a55362dfcd8fe00
Artur <art05xxx@rambler.ru>
2021-09-13 06:56:40 +0300

    9 commit created views

---
+++ web/chat/views.py
@@ -0,0 +1,33 @@
+import logging
+from django.utils.translation import gettext_lazy as _
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.reverse import reverse_lazy
+
+from main.service import BlogMicroService
+
+from . import services
+from . import serializers
+from django.shortcuts import render
+logger = logging.getLogger(__name__)
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
+
+
+class ShortUserInfoView(GenericAPIView):
+    # serializer_class = ShortUserInfoSerializer
+
+    def get(self, request, pk):
+        url = reverse_lazy('chat:short_user_info', kwargs={'pk': pk})
+        service = BlogMicroService(request, url)
+        return service.service_response()
