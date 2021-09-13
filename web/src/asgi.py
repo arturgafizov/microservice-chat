8798dca7c15c690e1e82b6b718da352e5a533873
Artur <art05xxx@rambler.ru>
2021-09-13 06:52:43 +0300

    4 commit Websoket

--- web/src/asgi.py
+++ web/src/asgi.py
@@ -3,6 +3,8 @@ import os
 from django.core.asgi import get_asgi_application
 from django.conf import settings
 
+import chat.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 django_asgi_app = get_asgi_application()
 
@@ -14,13 +16,18 @@ django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet.
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import OriginValidator
-from sockets.routing import websocket_urlpatterns as chart_urlpatterns
+# from sockets.routing import websocket_urlpatterns as chart_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    'websocket': AuthMiddlewareStack(
+    # 'websocket': AuthMiddlewareStack(
+    #     URLRouter(
+    #         chart_urlpatterns,
+    #     )
+    # ),
+    "websocket": AuthMiddlewareStack(
         URLRouter(
-            chart_urlpatterns,
+            chat.routing.websocket_urlpatterns
         )
     ),
 })
