3f87aa6e7fad41fb681dd93685c4e4b9813a0200
Artur <art05xxx@rambler.ru>
2021-09-17 07:28:10 +0300

    12 commit added urls for microservice django-blog

--- web/chat/urls.py
+++ web/chat/urls.py
@@ -11,10 +11,16 @@ router = DefaultRouter()
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
-    path('short/info/<pk>/', views.ShortUserInfoView.as_view(), name='short_user_info')
+    path('short/info/<pk>/', views.ShortUserInfoView.as_view(), name='short_user_info'),
+    path('short/info/', views.ListShortUserInfoView.as_view(), name='list_short_user_info'),
+    path('sign-in/info/<pk>/', views.UserSignInInfoView.as_view(), name='user_sign_in_info'),
+    path('sign-up/info/<pk>/', views.UserSignUpInfoView.as_view(), name='user_sign_up_info'),
 ]
 
 urlpatterns += router.urls
 urlpatterns += [
-    path('profiles/profile/short/<pk>/', TemplateView.as_view(), name='short_user_info')
+    path('profiles/profile/short/<pk>/', TemplateView.as_view(), name='short_user_info'),
+    path('profiles/profile/short/', TemplateView.as_view(), name='list_short_user_info'),
+    path('auth/sign-in/info/<pk>/', TemplateView.as_view(), name='user_sign_in_info'),
+    path('auth/sign-up/info/<pk>/', TemplateView.as_view(), name='user_sign_up_info'),
 ]
