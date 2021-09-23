8644a23a947b5a421d2f4f07017e694c18771e35
Artur <art05xxx@rambler.ru>
2021-09-23 14:32:59 +0300

    14 added inline function

--- web/chat/admin.py
+++ web/chat/admin.py
@@ -4,10 +4,15 @@ from django.contrib import admin
 from . models import Chat, UserChat, Message
 
 
+class UserChatInline(admin.TabularInline):
+    model = UserChat
+    extra = 0
+
+
 @admin.register(Chat)
 class ChatAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
-
+    inlines = (UserChatInline, )
 
 @admin.register(UserChat)
 class UserChatAdmin(admin.ModelAdmin):
