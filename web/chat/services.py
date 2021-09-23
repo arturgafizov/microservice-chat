15e4d0ba7ae77b19726343f5d9af24f804503b4d
Artur <art05xxx@rambler.ru>
2021-09-23 14:35:18 +0300

    18 created get_user_chat

--- web/chat/services.py
+++ web/chat/services.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 
 from . import models
+from .models import Chat
 
 
 class ChatService:
-    pass
+    @staticmethod
+    def get_user_chat(user_id:int):
+        return Chat.objects.select_related('user_chat').filter(user_chat__user_id=user_id)
