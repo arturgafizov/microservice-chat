7a1996517ea928ffc6333f1fc2f72525248bcb0c
Artur <art05xxx@rambler.ru>
2021-09-23 14:34:51 +0300

    17 created UserChatSerializer and ChatSerializer

--- web/chat/serializers.py
+++ web/chat/serializers.py
@@ -1,4 +1,19 @@
 from rest_framework import serializers
-
+from django.contrib.auth import get_user_model
 # Create your serializers here.
+from chat.models import Chat, UserChat
 
+
+class  UserChatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserChat
+        fields = ('user_id', )
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    user_chat = UserChatSerializer(many=True)
+
+    class Meta:
+        model = Chat
+        fields = ('id', 'name', 'user_chat')
