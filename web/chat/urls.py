5e0b8aba6b44afcfa2d014d21efcbf5ed270cc92
Artur <art05xxx@rambler.ru>
2021-09-23 14:36:12 +0300

    19 created urls for APi Chat list

--- web/chat/urls.py
+++ web/chat/urls.py
@@ -9,12 +9,11 @@ app_name = 'chat'
 router = DefaultRouter()
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-    path('short/info/<pk>/', views.ShortUserInfoView.as_view(), name='short_user_info'),
-    path('short/info/', views.ListShortUserInfoView.as_view(), name='list_short_user_info'),
-    path('sign-in/info/<pk>/', views.UserSignInInfoView.as_view(), name='user_sign_in_info'),
-    path('sign-up/info/<pk>/', views.UserSignUpInfoView.as_view(), name='user_sign_up_info'),
+    # path('short/info/<pk>/', views.ShortUserInfoView.as_view(), name='short_user_info'),
+    path('chat/list/', views.ChatListView.as_view(), name='chat_list_user'),
+    # path('short/info/', views.ListShortUserInfoView.as_view(), name='list_short_user_info'),
+    # path('sign-in/info/<pk>/', views.UserSignInInfoView.as_view(), name='user_sign_in_info'),
+    # path('sign-up/info/<pk>/', views.UserSignUpInfoView.as_view(), name='user_sign_up_info'),
 ]
 
 urlpatterns += router.urls
--- web/main/urls.py
+++ web/main/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic import RedirectView
-from .views import SetUserTimeZone
+from .views import SetUserTimeZone, TemplateAPIView
 
 
 urlpatterns = [
-    path('', login_required(RedirectView.as_view(pattern_name='admin:index'))),
+    path('', TemplateAPIView.as_view(template_name='chat/index.html'), name='chat'),
     path('timezone/', SetUserTimeZone.as_view(), name='set_user_timezone'),
 
 ]
