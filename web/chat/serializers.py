4dba06f7f2223dd65a64d25b3db000b6ed619767
Artur <art05xxx@rambler.ru>
2021-09-26 13:19:55 +0300

    25 add field last_message

--- web/chat/serializers.py
+++ web/chat/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 # Create your serializers here.
-from chat.models import Chat, UserChat
+from chat.models import Chat, UserChat, Message
+from . services import ChatService
 
 
 class  UserChatSerializer(serializers.ModelSerializer):
@@ -13,7 +14,18 @@ class  UserChatSerializer(serializers.ModelSerializer):
 
 class ChatSerializer(serializers.ModelSerializer):
     user_chat = UserChatSerializer(many=True)
+    last_message = serializers.SerializerMethodField('get_last_message')
 
     class Meta:
         model = Chat
-        fields = ('id', 'name', 'user_chat')
+        fields = ('id', 'name', 'user_chat' , 'last_message')
+
+    def get_last_message(self, obj):
+        return obj.last_message
+
+
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = ('author_id', 'content', 'chat', 'date')
