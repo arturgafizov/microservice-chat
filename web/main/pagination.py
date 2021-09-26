780c332ee41d6cb02b6ae56c780801df9585e878
Artur <art05xxx@rambler.ru>
2021-09-26 13:18:32 +0300

    24 delete def

--- web/main/pagination.py
+++ web/main/pagination.py
@@ -7,7 +7,3 @@ class BasePageNumberPagination(PageNumberPagination):
     max_page_size = 100
     page_size_query_param = 'page_size'
 
-    def get_paginated_response(self, data):
-        response = super().get_paginated_response(data)
-        response.data.update(self.get_html_context())
-        return response
