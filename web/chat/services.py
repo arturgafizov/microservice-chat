ddd81581ecfea9ed9e6feb9f0f6014e9756f8742
Artur <art05xxx@rambler.ru>
2021-09-26 13:21:25 +0300

    26 add get_user_chat Subquery last_message and create def get_messages

--- web/chat/services.py
+++ web/chat/services.py
@@ -1,10 +1,16 @@
 from django.conf import settings
-
+from django.db.models import Subquery, OuterRef
 from . import models
-from .models import Chat
+from .models import Chat, Message
 
 
 class ChatService:
     @staticmethod
     def get_user_chat(user_id:int):
-        return Chat.objects.select_related('user_chat').filter(user_chat__user_id=user_id)
+        return Chat.objects.prefetch_related('user_chat').filter(user_chat__user_id=user_id).annotate(
+            last_message=Subquery(Message.objects.filter(chat_id=OuterRef('id')).order_by('-date').values('content')[:1])
+        )
+
+    @staticmethod
+    def get_messages(chat_id: str):
+        return Message.objects.filter(chat_id=chat_id)
