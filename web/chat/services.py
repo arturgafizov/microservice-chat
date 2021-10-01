5d544bfa13b75fa2f7abc0600c9a39e55896a308
Artur <art05xxx@rambler.ru>
2021-10-01 09:53:01 +0300

    35 add Subquery update_date

--- web/chat/services.py
+++ web/chat/services.py
@@ -8,8 +8,10 @@ class ChatService:
     @staticmethod
     def get_user_chat(user_id:int):
         return Chat.objects.prefetch_related('user_chat').filter(user_chat__user_id=user_id).annotate(
-            last_message=Subquery(Message.objects.filter(chat_id=OuterRef('id')).order_by('-date').values('content')[:1])
-        )
+            last_message=Subquery(Message.objects.filter(chat_id=OuterRef('id')).order_by('-date').values('content')
+                                  [:1]),
+            update_date=Subquery(Message.objects.filter(chat_id=OuterRef('id')).order_by('-date').values('date')
+                                 [:1]))
 
     @staticmethod
     def get_messages(chat_id: str):
