bcbaa3af8fe39793f77fd158008544cf96bcf9bc
Artur <art05xxx@rambler.ru>
2021-11-07 22:00:48 +0300

    52 update UserChatSerializer, ChatSerializer, ChatInitSerializer

--- web/chat/serializers.py
+++ web/chat/serializers.py
@@ -3,29 +3,39 @@ from django.contrib.auth import get_user_model
 # Create your serializers here.
 from chat.models import Chat, UserChat, Message
 from . services import ChatService
+from main.utils import find_dict_in_list
 
 
-class  UserChatSerializer(serializers.ModelSerializer):
+class UserChatSerializer(serializers.ModelSerializer):
+    user_data = serializers.SerializerMethodField('get_user_data')
 
     class Meta:
         model = UserChat
-        fields = ('user_id', )
+        fields = ('user_id', 'user_data', )
+
+    def get_user_data(self, obj) -> dict:
+        # print(obj.user_id)
+        return find_dict_in_list(self.context['user_data'], 'id', obj.user_id)
 
 
 class ChatSerializer(serializers.ModelSerializer):
-    user_chat = UserChatSerializer(many=True)
+    user_chat = serializers.SerializerMethodField('get_user_chat')
     last_message = serializers.SerializerMethodField('get_last_message')
     update_date = serializers.SerializerMethodField('get_update_date')
 
     class Meta:
         model = Chat
-        fields = ('id', 'name', 'user_chat' , 'last_message', 'update_date')
+        fields = ('id', 'name', 'user_chat', 'last_message', 'update_date')
 
     def get_last_message(self, obj):
         return obj.last_message
 
     def get_update_date(self, obj):
         return obj.update_date
+
+    def get_user_chat(self, obj):
+        # print(self.context)
+        return UserChatSerializer(obj.user_chat, many=True, context=self.context).data
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -37,11 +47,17 @@ class MessageSerializer(serializers.ModelSerializer):
 
 class ChatInitSerializer(serializers.Serializer):
     jwt = serializers.CharField()
+    user_id = serializers.IntegerField()
 
     def validate_jwt(self, jwt: str):
         self.user_data = ChatService.post_jwt(self.context['request'], jwt)
+        # print(self.user_data)
         return jwt
 
     def save(self):
-        print(self.validated_data)
-
+        # print(self.validated_data)
+        user_1 = self.user_data.get('id')
+        user_2 = self.validated_data.get('user_id')
+        # print(user_1, user_2)
+        chat = ChatService.get_or_create_chat(user_1, user_2)
+        print(chat)
