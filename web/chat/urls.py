7c5fd989807284c4d07eb71975b3ecf9952869ce
Artur <art05xxx@rambler.ru>
2021-11-07 22:03:47 +0300

    55 add url user/users-id/

--- web/chat/urls.py
+++ web/chat/urls.py
@@ -13,7 +13,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     # path('short/info/<pk>/', views.ShortUserInfoView.as_view(), name='short_user_info'),
-    path('chat/list/', views.ChatListView.as_view(), name='chat_list_user'),
+    path('list/', views.ChatListView.as_view(), name='chat_list_user'),
     path('message/<chat_id>/', views.MessageListView.as_view(), name='message_list_user'),
     path('init/', ChatInitView.as_view(), name='init_template'),
     path('jwt/', TemplateView.as_view(template_name='chat/init.html'), name='jwt_user'),
@@ -28,4 +28,5 @@ urlpatterns += [
     path('auth/sign-in/info/<pk>/', TemplateView.as_view(), name='user_sign_in_info'),
     path('auth/sign-up/info/<pk>/', TemplateView.as_view(), name='user_sign_up_info'),
     path('user/jwt/', TemplateView.as_view(), name='user_jwt'),
+    path('user/users-id/', TemplateView.as_view(), name='users_id'),
 ]
