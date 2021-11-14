e2c87570483fb14f1277ce020d0e3ab53f33a281
Artur <art05xxx@rambler.ru>
2021-11-14 21:45:33 +0300

    62 create middleware.py

---
+++ web/chat/middleware.py
@@ -0,0 +1,19 @@
+from channels.sessions import CookieMiddleware
+from .services import ChatService
+
+
+class CookeAuthMiddlWare:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        user_jwt = scope['cookies'].get('jwt-auth')
+        if not user_jwt:
+            return
+        # print(user_data)
+        scope['user_data'] = ChatService.get_or_set_cache(None, user_jwt)
+        return await self.app(scope, receive, send)
+
+
+def AuthMiddlewareStack(inner):
+    return CookieMiddleware(CookeAuthMiddlWare(inner))
