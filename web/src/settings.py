92e043067782e62dc01333f007a64e67ec9c1a0e
Artur <art05xxx@rambler.ru>
2021-12-18 14:38:52 +0300

    90 commit

--- web/src/settings.py
+++ web/src/settings.py
@@ -103,9 +103,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'microservice_request.permissions.HasApiKeyOrIsAuthenticated',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.SessionAuthentication',
+    # ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
